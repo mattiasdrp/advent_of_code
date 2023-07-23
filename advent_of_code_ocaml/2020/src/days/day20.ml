8822ff2cd92491afd9a7eefca86022a83346e4e6
mattiasdrp <mattias@kountac.fr>
2023-07-23 16:29:24 +0200

    Day 20 of 2020 finished
    The solution deserves more comments to understand why the flipping and rotations
    act like this but it works really well

--- advent_of_code_ocaml/2020/src/days/day20.ml
+++ advent_of_code_ocaml/2020/src/days/day20.ml
@@ -1,139 +1,509 @@
 open Mdrp_lib
 
-module Square = struct
+let size_min = 1
+let size_max = 9
+let size_square = size_max - size_min
+
+module rec Square : sig
   type t = {
     id : int;
+    flip : bool;
     rotation : int;
-    north : string;
-    west : string;
-    east : string;
-    south : string;
+    north : int * int;
+    west : int * int;
+    east : int * int;
+    south : int * int;
+    content : string array;
+  }
+  (** A square has 4 sides. Each side is a pair of int because they can be read in both directions
+      and we need to remember both values.
+
+      The flip and rotation fields correspond to the transformations applied to the square *)
+
+  val create :
+    int ->
+    north:string ->
+    west:string ->
+    east:string ->
+    south:string ->
+    string list ->
+    t
+  (** Classic creation.
+
+      The only thing that happens here is the trimming of the content to remove the borders *)
+
+  val pp : ?minimal:bool -> unit -> Format.formatter -> t -> unit
+
+  val compare : t -> t -> int
+  (** We don't need a complex comparison function
+      since it's impossible that we add the same square that has been rotated or flipped *)
+
+  val equal : t -> t -> bool
+  (** Same as [!compare] *)
+
+  val get_row : int -> t -> string
+  (** [get_row row t] will return the row at index [row] from the content according to the transformations applied to [t]. *)
+
+  val transform : bool -> int -> t -> t
+  (** [transform flip rotate t] flips [t] according to [flip] then applies a clockwise rotation by steps of [rotate * 90]° *)
+
+  type touching = NS | EW | SN | WE
+
+  val transform_touching : t -> touching -> touching
+  val same_sides : t -> t -> touching list
+  val pp_touching : Format.formatter -> touching -> unit
+
+  val neighbours : t list -> touching list SquareMap.t SquareMap.t
+  (** For each square [t], for each possible flipping and rotation of a square [t'], checks if [t'] it can [t] *)
+end = struct
+  type t = {
+    id : int;
+    flip : bool;
+    rotation : int;
+    north : int * int;
+    west : int * int;
+    east : int * int;
+    south : int * int;
+    content : string array;
   }
 
-  let empty =
-    { id = -1; rotation = 0; north = ""; west = ""; east = ""; south = "" }
+  (** The value of a side *)
+  let value string =
+    let v, r, _ =
+      String.fold_left
+        (fun (value, reverse, rev_pow) -> function
+          | '.' -> (value lsl 1, reverse, rev_pow * 2)
+          | _ -> ((value lsl 1) + 1, reverse + rev_pow, rev_pow * 2))
+        (0, 0, 1) string
+    in
+    (v, r)
 
-  let compare t1 t2 =
-    let c = Int.compare t1.id t2.id in
-    if c <> 0 then c else Int.compare t1.rotation t2.rotation
+  let create id ~north ~west ~east ~south content =
+    let north = value north in
+    let west = value west in
+    let east = value east in
+    let south = value south in
+    let content =
+      List.map (fun s -> String.sub s size_min size_square) content
+      |> List.rev |> Array.of_list
+    in
+    { id; flip = false; rotation = 0; north; west; east; south; content }
 
+  let compare t1 t2 = Int.compare t1.id t2.id
   let equal t1 t2 = Int.equal t1.id t2.id
+
+  (** Flip the square then rotate it *)
+  let apply_transformation t =
+    match t.flip with
+    | true -> (
+        match t.rotation with
+        | 0 ->
+            {
+              t with
+              north = Pair.rev t.north;
+              south = Pair.rev t.south;
+              west = t.east;
+              east = t.west;
+            }
+        | 1 ->
+            {
+              t with
+              north = Pair.rev t.east;
+              east = Pair.rev t.north;
+              south = Pair.rev t.west;
+              west = Pair.rev t.south;
+            }
+        | 2 ->
+            {
+              t with
+              north = t.south;
+              south = t.north;
+              east = Pair.rev t.east;
+              west = Pair.rev t.west;
+            }
+        | 3 ->
+            {
+              t with
+              north = t.west;
+              east = t.south;
+              south = t.east;
+              west = t.north;
+            }
+        | _ -> assert false)
+    | false -> (
+        match t.rotation with
+        | 0 -> t
+        | 1 ->
+            {
+              t with
+              north = Pair.rev t.west;
+              east = t.north;
+              south = Pair.rev t.east;
+              west = t.south;
+            }
+        | 2 ->
+            {
+              t with
+              north = Pair.rev t.south;
+              east = Pair.rev t.west;
+              south = Pair.rev t.north;
+              west = Pair.rev t.east;
+            }
+        | 3 ->
+            {
+              t with
+              north = t.east;
+              east = Pair.rev t.south;
+              south = t.west;
+              west = Pair.rev t.north;
+            }
+        | _ -> assert false)
 
   let pp ?(minimal = false) () ppf t =
     if minimal then
-      Format.fprintf ppf "{id: %d, rot: %s}" t.id
+      Format.fprintf ppf "{id: %d, rot: %s; flip: %b}" t.id
         (match t.rotation with
-        | 0 -> "∅"
-        | 1 -> "↴"
-        | 2 -> "↵"
-        | 3 -> "↻"
+        | 0 -> "0"
+        | 1 -> "1"
+        | 2 -> "2"
+        | 3 -> "3"
         | _ -> assert false)
+        t.flip
     else
       Format.fprintf ppf
-        "{id = %d; rotation: %d; north = %s; west = %s; east = %s; south = %s }"
-        t.id t.rotation t.north t.west t.east t.south
+        "{id = %d; rotation = %d; flip: %5b; north = %3d; south = %3d; west = \
+         %3d; east = %3d }"
+        t.id t.rotation t.flip (fst t.north) (fst t.south) (fst t.west)
+        (fst t.east)
 
-  (** Clockwise rotation by 90° *)
-  let rotate t =
-    {
-      t with
-      rotation = t.rotation + (1 mod 4);
-      north = String.reverse t.west;
-      east = t.north;
-      south = String.reverse t.east;
-      west = t.south;
-    }
+  let max_index = size_square - 1
 
-  type touching = NS | SN | WE | EW
+  let get_row row t =
+    match t.flip with
+    | false -> (
+        match t.rotation with
+        | 0 -> t.content.(row)
+        | 2 -> String.reverse t.content.(max_index - row)
+        | 1 ->
+            let bytes = Bytes.create size_square in
+            for i = max_index downto 0 do
+              let s = Array.unsafe_get t.content i in
+              Bytes.unsafe_set bytes (max_index - i) s.[row]
+            done;
+            Bytes.to_string bytes
+        | 3 ->
+            let bytes = Bytes.create size_square in
+            for i = 0 to max_index do
+              let s = Array.unsafe_get t.content i in
+              Bytes.unsafe_set bytes i s.[max_index - row]
+            done;
+            Bytes.to_string bytes
+        | _ -> assert false)
+    | true -> (
+        match t.rotation with
+        | 0 -> String.reverse t.content.(row)
+        | 2 -> t.content.(max_index - row)
+        | 1 ->
+            let bytes = Bytes.create size_square in
+            for i = max_index downto 0 do
+              let s = Array.unsafe_get t.content i in
+              Bytes.unsafe_set bytes (max_index - i) s.[max_index - row]
+            done;
+            Bytes.to_string bytes
+        | 3 ->
+            let bytes = Bytes.create size_square in
+            for i = 0 to max_index do
+              let s = Array.unsafe_get t.content i in
+              Bytes.unsafe_set bytes i s.[row]
+            done;
+            Bytes.to_string bytes
+        | _ -> assert false)
+
+  let transform flip rotate t =
+    let rotation =
+      (* If the square is flipped, the rotation applied to it needs to be counterclockwise. *)
+      ((if flip then 4 - t.rotation else t.rotation) + rotate) mod 4
+    in
+    let flip = flip ||! t.flip in
+    apply_transformation { t with rotation; flip }
+
+  (** Type representing on which side two squares are touching *)
+  type touching = NS | EW | SN | WE
+
+  let to_int flip = function
+    | NS -> 0
+    | EW -> if flip then 3 else 1
+    | SN -> 2
+    | WE -> if flip then 1 else 3
+
+  let of_int = function
+    | 0 -> NS
+    | 1 -> EW
+    | 2 -> SN
+    | 3 -> WE
+    | _ -> assert false
+
+  let move flip i touching = of_int ((to_int flip touching + i) mod 4)
+
+  (** Transform a side-by-side touching according to the flipping and rotation of the parent square *)
+  let transform_touching t touching = move t.flip t.rotation touching
 
   let pp_touching ppf t =
     Format.fprintf ppf "%s"
       (match t with NS -> "NS" | SN -> "SN" | WE -> "WE" | EW -> "EW")
 
   let same_sides t1 t2 =
-    let rec aux s1 s2 touching acc = if s1 = s2 then touching :: acc else acc in
+    let aux s1 s2 touching acc = if s1 = s2 then touching :: acc else acc in
     aux t1.north t2.south NS []
     |> aux t1.south t2.north SN |> aux t1.west t2.east WE
     |> aux t1.east t2.west EW
+
+  let neighbours l =
+    let rec aux map = function
+      | s1 :: tl ->
+          let set =
+            List.fold_left
+              (fun set s2 ->
+                if equal s1 s2 then set
+                else
+                  let rotations = [ 0; 1; 2; 3 ] in
+                  let transformations =
+                    List.cartesian_product [ false; true ] rotations
+                  in
+                  List.fold_left
+                    (fun set (flip, rotate) ->
+                      let s2 = transform flip rotate s2 in
+                      let sides = same_sides s1 s2 in
+                      match sides with
+                      | [] -> set
+                      | _ -> SquareMap.add s2 sides set)
+                    set transformations)
+              SquareMap.empty l
+          in
+          aux (SquareMap.add s1 set map) tl
+      | [] -> map
+    in
+    aux SquareMap.empty l
 end
+
+and SquareMap : (Map.S with type key = Square.t) = Map.Make (struct
+  include Square
+
+  let pp ppf t = pp ~minimal:true () ppf t
+end)
+
+module SquareSet = Set.Make (struct
+  include Square
+
+  let pp = Square.pp ~minimal:true ()
+end)
 
 let parse file =
   let re_field = Str.regexp {|Tile \([0-9]+\):|} in
 
-  let _, _, acc =
+  let _, _, _, _, _, _, _, acc =
     Parse.fold_lines ~end_line:true
-      (fun (index, current, acc) line ->
+      (fun (index, id, north, west, east, south, lines, acc) line ->
         match line with
-        | "" -> (0, Square.empty, current :: acc)
-        | s ->
+        | "" ->
+            ( 0,
+              -1,
+              "",
+              "",
+              "",
+              "",
+              [],
+              Square.create id ~north ~west ~east ~south lines :: acc )
+        | _ ->
             (* First line giving us the id *)
-            if Str.string_match re_field s 0 then
+            if Str.string_match re_field line 0 then
               ( 0,
-                { current with id = int_of_string (Str.matched_group 1 s) },
+                int_of_string (Str.matched_group 1 line),
+                north,
+                west,
+                east,
+                south,
+                lines,
                 acc )
             else
-              let north = if index = 0 then line else current.north in
+              let north = if index = 0 then line else north in
               let south = line in
-              let west = String.append_char current.west line.[0] in
+              let west = String.append_char west line.[0] in
               let east =
-                String.append_char current.east line.[String.length line - 1]
+                String.append_char east line.[String.length line - 1]
               in
-              (index + 1, { current with north; west; east; south }, acc))
-      (0, Square.empty, []) file
+              let lines =
+                if index < size_min || index >= size_max then lines
+                else line :: lines
+              in
+              (index + 1, id, north, west, east, south, lines, acc))
+      (0, -1, "", "", "", "", [], [])
+      file
   in
-  Format.eprintf "@[<v 0>%a@."
-    List.(pp ~pp_sep:Format.pp_print_cut Square.(pp ()))
-    acc;
-  acc
-
-module SquareMap = Map.Make (struct
-  include Square
-
-  let pp = pp ~minimal:true ()
-end)
-
-let neighbours l =
-  let rec aux map = function
-    | s1 :: tl ->
-        let map =
-          List.fold_left
-            (fun map s2 ->
-              if Square.equal s1 s2 then map
-              else
-                let s2r = Square.rotate s2 in
-                let s2rr = Square.rotate s2r in
-                let s2rrr = Square.rotate s2rr in
-                let l2 = [ s2; s2r; s2rr; s2rrr ] in
-                List.fold_left
-                  (fun map s2 ->
-                    let sides = Square.same_sides s1 s2 in
-                    match sides with
-                    | [] -> map
-                    | _ ->
-                        SquareMap.update s1
-                          (function
-                            | None -> Some (SquareMap.singleton s2 sides)
-                            | Some m -> Some (SquareMap.add s2 sides m))
-                          map)
-                  map l2)
-            map l
-        in
-        aux map tl
-    | [] -> map
-  in
-  aux SquareMap.empty l
+  Square.neighbours acc
 
 let part_1 file =
-  let squares = parse file in
-  let map = neighbours squares in
-  Format.eprintf "@[<v 0>%a@."
-    SquareMap.(
-      pp (fun ppf s ->
-          Format.fprintf ppf "@[<v 0>%a@]"
-            SquareMap.(pp List.(pp Square.(pp_touching)))
-            s))
-    map;
-  0
+  let neighbours = parse file in
+  SquareMap.fold
+    (fun s map angles ->
+      if SquareMap.cardinal map = 2 then angles * s.Square.id else angles)
+    neighbours 1
 
-let part_2 _file = failwith "TODO"
+module Image = struct
+  type t = { rows : int; cols : int; content : string array }
+
+  let pp ppf t =
+    Format.fprintf ppf "{rows: %d; cols: %d; content: %a}" t.rows t.cols
+      Array.(pp String.pp)
+      t.content
+
+  let sharps t =
+    Array.fold_left
+      (fun acc s ->
+        String.fold_left (fun acc -> function '#' -> acc + 1 | _ -> acc) acc s)
+      0 t.content
+end
+
+let recompose_image neighbours =
+  let boarders =
+    SquareMap.fold
+      (fun _s map boarders ->
+        if SquareMap.cardinal map <= 3 then boarders + 1 else boarders)
+      neighbours 0
+  in
+  let angle, _ =
+    SquareMap.find_predicate
+      (fun _s map ->
+        SquareMap.cardinal map = 2
+        &&
+        match SquareMap.bindings map with
+        | [ (_, [ Square.SN ]); (_, [ EW ]) ] | [ (_, [ EW ]); (_, [ SN ]) ] ->
+            true
+        | _ -> false)
+      neighbours
+  in
+  let image = Array.init (size_square * ((boarders / 4) + 1)) (fun _ -> "") in
+  let queue = Queue.create () in
+  let next_rows = Queue.create () in
+  Queue.push (angle, (0, 0)) queue;
+  while not (Queue.is_empty queue && Queue.is_empty next_rows) do
+    let square, (row, col) =
+      try Queue.pop queue with Queue.Empty -> Queue.pop next_rows
+    in
+    for i = 0 to size_square - 1 do
+      let s = Square.get_row i square in
+      let i_image = (size_square * row) + i in
+      let is = Array.unsafe_get image i_image in
+      Array.unsafe_set image i_image (is ^ s)
+    done;
+    let nbrs = SquareMap.find square neighbours in
+    SquareMap.iter
+      (fun neighbour sides ->
+        (* Don't forget to flip and rotate the touching sides according to the parent square *)
+        match List.map (Square.transform_touching square) sides with
+        | [ Square.NS ] | [ WE ] -> ()
+        | [ SN ] when col = 0 ->
+            (* transform the next square according to its parent flipping and rotation *)
+            let neighbour =
+              Square.transform square.flip square.rotation neighbour
+            in
+            Queue.add (neighbour, (row + 1, 0)) next_rows
+        | [ EW ] ->
+            (* transform the next square according to its parent flipping and rotation *)
+            let neighbour =
+              Square.transform square.flip square.rotation neighbour
+            in
+            Queue.add (neighbour, (row, col + 1)) queue
+        | [ SN ] -> ()
+        | _ -> assert false)
+      nbrs
+  done;
+  Image.
+    {
+      rows = Array.length image;
+      cols = String.length image.(0);
+      content = image;
+    }
+
+let flip (Image.{ rows; content; _ } as t) =
+  let ncontent = Array.copy content in
+  for i = 0 to (rows - 1) / 2 do
+    ncontent.(i) <- content.(rows - 1 - i);
+    ncontent.(rows - 1 - i) <- content.(i)
+  done;
+  { t with content = ncontent }
+
+let rotate Image.{ rows; cols; content } =
+  let content =
+    Array.init cols (fun col ->
+        String.init rows (fun row -> content.(rows - 1 - row).[col]))
+  in
+  Image.{ rows = cols; cols = rows; content }
+
+let check_monster image row_i col_i monster =
+  let open Image in
+  let rec aux row col =
+    row < image.rows && col < image.cols
+    && (row = monster.rows
+       ||
+       if col = monster.cols then aux (row + 1) 0
+       else
+         match monster.content.(row).[col] with
+         | ' ' -> aux row (col + 1)
+         | '#' -> (
+             match image.content.(row_i + row).[col_i + col] with
+             | '#' -> aux row (col + 1)
+             | _ | (exception Invalid_argument _) -> false)
+         | _ | (exception Invalid_argument _) -> assert false)
+  in
+  aux 0 0
+
+let number_of_monsters image monster =
+  let open Image in
+  let rec aux row col res =
+    if row >= image.rows - monster.rows then res
+    else if col >= image.cols - monster.cols then aux (row + 1) 0 res
+    else if check_monster image row col monster then aux row (col + 1) (res + 1)
+    else aux row (col + 1) res
+  in
+  aux 0 0 0
+
+let apply n rotate image =
+  let rec aux i image = if i = 0 then image else aux (i - 1) (rotate image) in
+  aux n image
+
+let find_monsters image monster =
+  let rotations = [ 0; 1; 2; 3 ] in
+  let transformations = List.cartesian_product [ false; true ] rotations in
+  let rec aux = function
+    | [] -> assert false
+    | (f, r) :: tl ->
+        let monster = (if f then flip monster else monster) |> apply r rotate in
+        let res = number_of_monsters image monster in
+        if res > 0 then res else aux tl
+  in
+  aux transformations
+
+let part_2 file =
+  let neighbours = parse file in
+  let image = recompose_image neighbours in
+  let sharps_image = Image.sharps image in
+
+  let monster =
+    Image.
+      {
+        rows = 3;
+        cols = 20;
+        content =
+          [|
+            "                  # ";
+            "#    ##    ##    ###";
+            " #  #  #  #  #  #   ";
+          |];
+      }
+  in
+  let sharps_monster = Image.sharps monster in
+  let monsters = find_monsters image monster in
+  sharps_image - (monsters * sharps_monster)
+
 let run part file = match part with 1 -> part_1 file | _ -> part_2 file
