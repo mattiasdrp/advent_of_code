80582df3738e7e3589cab63580102e4460749244
mattiasdrp <mattias@kountac.fr>
2023-07-28 14:07:21 +0200

    Day 8 and 9 of 2015

--- advent_of_code_ocaml/2015/src/days/day08.ml
+++ advent_of_code_ocaml/2015/src/days/day08.ml
@@ -1,3 +1,40 @@
-let part_1 _file = failwith "TODO"
-let part_2 _file = failwith "TODO"
+open Mdrp_lib
+
+let parse line =
+  let code_length = String.length line in
+  let real_length, _, _ =
+    String.fold_left
+      (fun (acc, escaped, skip) -> function
+        | ('\\' | '"') when escaped -> (acc + 1, false, 0)
+        | '\\' -> (acc, true, 0)
+        | '"' -> (acc, false, 0)
+        | 'x' when escaped -> (acc, false, 1)
+        | _ when skip = 1 -> (acc, false, 0)
+        | _ -> (acc + 1, false, 0))
+      (0, false, 0) line
+  in
+  (code_length, real_length)
+
+let part_1 file =
+  let code_lengths, real_lengths =
+    Parse.fold_lines
+      (fun (code_lengths, real_lengths) line ->
+        let code_length, real_length = parse line in
+        (code_length + code_lengths, real_length + real_lengths))
+      (0, 0) file
+  in
+  code_lengths - real_lengths
+
+let parse line = (String.length line, 2 + (String.length @@ String.escaped line))
+
+let part_2 file =
+  let orig_lengths, escaped_lengths =
+    Parse.fold_lines
+      (fun (orig_lengths, escaped_lengths) line ->
+        let orig_length, escaped_length = parse line in
+        (orig_length + orig_lengths, escaped_length + escaped_lengths))
+      (0, 0) file
+  in
+  escaped_lengths - orig_lengths
+
 let run part file = match part with 1 -> part_1 file | _ -> part_2 file
--- advent_of_code_ocaml/2015/src/days/day09.ml
+++ advent_of_code_ocaml/2015/src/days/day09.ml
@@ -1,3 +1,51 @@
-let part_1 _file = failwith "TODO"
-let part_2 _file = failwith "TODO"
+open Mdrp_lib
+
+module PMap = struct
+  include Map.Make (Pair.Make (String) (String))
+
+  let add (s1, s2) v t =
+    if String.compare s1 s2 < 0 then add (s1, s2) v t else add (s2, s1) v t
+
+  let find (s1, s2) t =
+    if String.compare s1 s2 < 0 then find (s1, s2) t else find (s2, s1) t
+end
+
+let parse (towns, map) line =
+  let re = Str.regexp {|\([a-zA-Z]+\) to \([a-zA-Z]+\) = \([0-9]+\)|} in
+  if Str.string_match re line 0 then
+    let town1 = Str.matched_group 1 line in
+    let town2 = Str.matched_group 2 line in
+    let dist = int_of_string @@ Str.matched_group 3 line in
+    ( String.Set.add town1 (String.Set.add town2 towns),
+      PMap.add (town1, town2) dist map )
+  else assert false
+
+let dist map l =
+  let rec aux acc = function
+    | [ _ ] -> acc
+    | t1 :: (t2 :: _ as tl) ->
+        let dist = PMap.find (t1, t2) map in
+        aux (acc + dist) tl
+    | _ -> assert false
+  in
+  aux 0 l
+
+let part_1 file =
+  let towns, map = Parse.fold_lines parse (String.Set.empty, PMap.empty) file in
+  let permutations = Seq.permutations (String.Set.elements towns) in
+  Seq.fold_left
+    (fun min_dist l ->
+      let dist = dist map l in
+      min dist min_dist)
+    max_int permutations
+
+let part_2 file =
+  let towns, map = Parse.fold_lines parse (String.Set.empty, PMap.empty) file in
+  let permutations = Seq.permutations (String.Set.elements towns) in
+  Seq.fold_left
+    (fun max_dist l ->
+      let dist = dist map l in
+      max dist max_dist)
+    0 permutations
+
 let run part file = match part with 1 -> part_1 file | _ -> part_2 file
