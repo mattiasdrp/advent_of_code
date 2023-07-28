1010d208d2796c962dcbf457214c5e58111541c0
mattiasdrp <mattias@kountac.fr>
2023-07-28 15:18:02 +0200

    Day 10 of 2015

--- advent_of_code_ocaml/2015/src/days/day10.ml
+++ advent_of_code_ocaml/2015/src/days/day10.ml
@@ -1,3 +1,33 @@
-let part_1 _file = failwith "TODO"
-let part_2 _file = failwith "TODO"
+open Mdrp_lib
+
+let read reversed l =
+  let rec aux prev cpt acc = function
+    | [] -> if reversed then cpt :: prev :: acc else prev :: cpt :: acc
+    | hd :: tl when hd = prev -> aux prev (cpt + 1) acc tl
+    | hd :: tl ->
+        aux hd 1
+          (if reversed then cpt :: prev :: acc else prev :: cpt :: acc)
+          tl
+  in
+  aux (List.hd l) 1 [] (List.tl l)
+
+let loop n l =
+  let rec aux i reversed l =
+    if i = n then List.length l
+    else aux (i + 1) (not reversed) (read reversed l)
+  in
+  aux 0 false l
+
+let parse file =
+  let ci = open_in file in
+  let line =
+    input_line ci
+    |> String.fold_left (fun acc c -> Char.to_digit c :: acc) []
+    |> List.rev
+  in
+  close_in ci;
+  line
+
+let part_1 file = loop 40 (parse file)
+let part_2 file = loop 50 (parse file)
 let run part file = match part with 1 -> part_1 file | _ -> part_2 file
--- advent_of_code_ocaml/2015/test/main.ml
+++ advent_of_code_ocaml/2015/test/main.ml
@@ -34,5 +34,53 @@ let () =
   Log.info "Day 07, part 2, is correct.";
   unit
 
+let () =
+  Test.register ~__FILE__ ~title:"d08.p1" ~tags:[ "d08"; "p1"; "fast" ]
+  @@ fun () ->
+  Check.((Day08.run 1 "resources/day08" = 1371) int)
+    ~error_msg:"expected Day08.run 1 input = %R, got %L";
+  Log.info "Day 08, part 1, is correct.";
+  unit
+
+let () =
+  Test.register ~__FILE__ ~title:"d08.p2" ~tags:[ "d08"; "p2"; "fast" ]
+  @@ fun () ->
+  Check.((Day08.run 2 "resources/day08" = 2117) int)
+    ~error_msg:"expected Day08.run 2 input = %R, got %L";
+  Log.info "Day 08, part 2, is correct.";
+  unit
+
+let () =
+  Test.register ~__FILE__ ~title:"d09.p1" ~tags:[ "d09"; "p1"; "fast" ]
+  @@ fun () ->
+  Check.((Day09.run 1 "resources/day09" = 207) int)
+    ~error_msg:"expected Day09.run 1 input = %R, got %L";
+  Log.info "Day 09, part 1, is correct.";
+  unit
+
+let () =
+  Test.register ~__FILE__ ~title:"d09.p2" ~tags:[ "d09"; "p2"; "fast" ]
+  @@ fun () ->
+  Check.((Day09.run 2 "resources/day09" = 804) int)
+    ~error_msg:"expected Day09.run 2 input = %R, got %L";
+  Log.info "Day 09, part 2, is correct.";
+  unit
+
+let () =
+  Test.register ~__FILE__ ~title:"d10.p1" ~tags:[ "d10"; "p1"; "fast" ]
+  @@ fun () ->
+  Check.((Day10.run 1 "resources/day10" = 360154) int)
+    ~error_msg:"expected Day10.run 1 input = %R, got %L";
+  Log.info "Day 10, part 1, is correct.";
+  unit
+
+let () =
+  Test.register ~__FILE__ ~title:"d10.p2" ~tags:[ "d10"; "p2"; "slow" ]
+  @@ fun () ->
+  Check.((Day10.run 2 "resources/day10" = 5103798) int)
+    ~error_msg:"expected Day10.run 2 input = %R, got %L";
+  Log.info "Day 10, part 2, is correct.";
+  unit
+
 (* CALL the main function of Tezt so that it actually runs your tests. *)
 let () = Test.run ()
