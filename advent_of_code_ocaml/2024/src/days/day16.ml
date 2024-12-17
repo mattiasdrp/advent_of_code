36cee9024e8e1e172b3328363b4dd8dab20727e0
mattiasdrp <mattias@kojin.tech>
2024-12-17 17:33:12 +0100

    Day 16 and 17 (incomplete) of 2024

--- advent_of_code_ocaml/2024/src/days/day16.ml
+++ advent_of_code_ocaml/2024/src/days/day16.ml
@@ -1,10 +1,10 @@
 open Mdrp_lib
 
 module Cell = struct
-  type t = Empty of int | Wall | Reindeer | End
+  type t = Empty | Wall | Reindeer | End
 
   let of_char = function
-    | '.' -> Empty max_int
+    | '.' -> Empty
     | '#' -> Wall
     | 'S' -> Reindeer
     | 'E' -> End
@@ -12,19 +12,73 @@ module Cell = struct
 
   let pp ppf t =
     Format.fprintf ppf "%c"
-      (match t with
-      | Empty _ -> '.'
-      | Wall -> '#'
-      | Reindeer -> 'S'
-      | End -> 'E')
+      (match t with Empty -> '.' | Wall -> '#' | Reindeer -> 'S' | End -> 'E')
 end
 
-module Coords = struct
+module Dir = struct
+  type t = North | East | South | West
+
+  let get_deltas = function
+    | North -> (-1, 0)
+    | East -> (0, 1)
+    | South -> (1, 0)
+    | West -> (0, -1)
+
+  let rotate_counterclockwise dir =
+    match dir with
+    | North -> West
+    | East -> North
+    | South -> East
+    | West -> South
+
+  let rotate_clockwise dir =
+    match dir with
+    | North -> East
+    | East -> South
+    | South -> West
+    | West -> North
+
+  let compare = Stdlib.compare
+
+  let pp ppf t =
+    Format.fprintf ppf "%s"
+      (match t with
+      | North -> "North"
+      | West -> "West"
+      | East -> "East"
+      | South -> "South")
+end
+
+module Coord = struct
   type t = { row : int; col : int }
 
   let compare = compare
-  let move { row; col } (drow, dcol) = { row = row + drow; col = col + dcol }
   let pp ppf { row; col } = Format.fprintf ppf "(%d, %d)" row col
+end
+
+module Location = struct
+  open Coord
+
+  type t = { coord : Coord.t; dir : Dir.t; coords : Coord.t list }
+
+  let compare t1 t2 =
+    let c1 = Coord.compare t1.coord t2.coord in
+    if c1 = 0 then Dir.compare t1.dir t2.dir else c1
+
+  let move { coord = { row; col }; dir; coords } =
+    let drow, dcol = Dir.get_deltas dir in
+    let coord = { row = row + drow; col = col + dcol } in
+    { coord; dir; coords = coord :: coords }
+
+  let move_clockwise t = move { t with dir = Dir.rotate_clockwise t.dir }
+
+  let move_counterclockwise t =
+    move { t with dir = Dir.rotate_counterclockwise t.dir }
+
+  let pp ppf { coord = { row; col }; dir; coords } =
+    Format.fprintf ppf "(%d, %d, %a) %a" row col Dir.pp dir
+      List.(pp Coord.pp)
+      coords
 end
 
 module Matrix = struct
@@ -51,164 +105,89 @@ module Matrix = struct
           match cell with Cell.Reindeer -> (row, col) | _ -> pos_reindeer)
         (0, 0) t
     in
-    Coords.{ row; col }
+    let coord = Coord.{ row; col } in
+    Location.{ coord; dir = East; coords = [ coord ] }
 end
 
-module Dir = struct
-  type t = North | East | South | West
+module Visited = Map.Make (Location)
 
-  let rotate_left dir =
-    match dir with
-    | North -> ((0, -1), West)
-    | East -> ((-1, 0), North)
-    | South -> ((0, 1), East)
-    | West -> ((1, 0), South)
+module Queue = struct
+  include Set.Make (struct
+    type t = Location.t * int
 
-  let rotate_right dir =
-    match dir with
-    | North -> ((0, 1), East)
-    | East -> ((1, 0), South)
-    | South -> ((0, -1), West)
-    | West -> ((-1, 0), North)
+    let compare (location1, score1) (location2, score2) =
+      let c1 = compare score1 score2 in
+      if c1 = 0 then Location.compare location1 location2 else c1
 
-  let stay = function
-    | North -> (-1, 0)
-    | East -> (0, 1)
-    | South -> (1, 0)
-    | West -> (0, -1)
+    let pp ppf (loc, score) =
+      Format.fprintf ppf "(%a: %d)" Location.pp loc score
+  end)
 
-  let compare = Stdlib.compare
-  let all_dirs dir = (stay dir, rotate_left dir, rotate_right dir)
+  let add ((loc, score) as elt) t =
+    match find elt t with
+    | loc', _ ->
+        add
+          ({ loc with coords = List.rev_append loc.coords loc'.coords }, score)
+          (remove elt t)
+    | exception _ -> add elt t
 
-  let pp ppf t =
-    Format.fprintf ppf "%s"
-      (match t with
-      | North -> "North"
-      | West -> "West"
-      | East -> "East"
-      | South -> "South")
+  let pop queue =
+    let elt = min_elt queue in
+    (elt, remove elt queue)
 end
 
-module Coord_dir = struct
-  type t = Coords.t * Dir.t
+let is_end grid location =
+  grid.(location.Location.coord.row).(location.coord.col) = Cell.End
 
-  let pp _ _ = ()
-  let compare = compare
-end
+let add_if_empty location score grid queue =
+  if grid.(location.Location.coord.row).(location.coord.col) != Cell.Wall then
+    Queue.add (location, score) queue
+  else queue
 
-module Visited = Set.Make (Coord_dir)
-
-module Element = struct
-  type t = { dist : int; pos : Coords.t; dir : Dir.t; path : Coord_dir.t list }
-
-  let pp ppf { dist; pos; dir; _ } =
-    Format.fprintf ppf "{dist: %d; pos: %a; dir: %a}" dist Coords.pp pos Dir.pp
-      dir
-
-  let compare t1 t2 =
-    let c = compare t1.dist t2.dist in
-    if c = 0 then
-      let c1 = compare t1.pos t2.pos in
-      if c1 = 0 then
-        let c2 = compare t1.dir t2.dir in
-        if c2 = 0 then List.compare Coords.compare t1.path t2.path else c2
-      else c1
-    else c
-end
-
-module Queue = Set.Make (Element)
-module Min_paths = Set.Make (Coord_dir)
+module Spots = Set.Make (Coord)
 
 let traverse grid start =
-  let open Element in
-  let rec aux queue visited ((min_dist, min_paths) as mins) =
-    (* Format.eprintf "%a@." Coords.pp pos; *)
-    (* Format.eprintf "Queue: @[<v 0>%a@." *)
-    (*   Queue.(pp ~pp_sep:Format.pp_print_cut ()) *)
-    (*   queue; *)
-    if Queue.is_empty queue then mins
+  let rec aux queue visited all_paths registered_score =
+    if Queue.is_empty queue then (registered_score, all_paths)
     else
-      let next = Queue.min_elt queue in
-      (* Format.eprintf "Min_dist: %d@.Next:@.  %a@." min_dist Element.pp next; *)
-      let queue = Queue.remove next queue in
-      let queue, visited, mins =
-        if grid.(next.pos.row).(next.pos.col) = Cell.End then
-          (* Format.eprintf "Reached end!@."; *)
-          let mins =
-            if next.dist < min_dist then
-              (* Format.eprintf "New min!@."; *)
-              (next.dist, Min_paths.of_list next.path)
-            else if next.dist = min_dist then
-              ( (* Format.eprintf "Adding to min!@."; *)
-                min_dist,
-                List.fold_left
-                  (fun acc pos -> Min_paths.add pos acc)
-                  min_paths next.path )
-            else mins
-          in
-          (queue, visited, mins)
-        else if
-          Visited.mem (next.pos, next.dir) visited
-          && Min_paths.mem (next.pos, next.dir) min_paths
-        then
-          ( queue,
-            visited,
-            ( min_dist,
-              List.fold_left
-                (fun acc pos -> Min_paths.add pos acc)
-                min_paths next.path ) )
-        else if
-          next.dist > min_dist
-          || grid.(next.pos.row).(next.pos.col) = Cell.Wall
-          || Visited.mem (next.pos, next.dir) visited
-        then (queue, visited, mins)
-        else
-          let visited = Visited.add (next.pos, next.dir) visited in
-          (* Format.eprintf "%a@." Cell.pp grid.(row).(col); *)
-          let stay_delta, (left_delta, left_dir), (right_delta, right_dir) =
-            Dir.all_dirs next.dir
-          in
-          let path = (next.pos, next.dir) :: next.path in
-          let queue =
-            Queue.add
-              {
-                next with
-                pos = Coords.move next.pos stay_delta;
-                dist = next.dist + 1;
-                path;
-              }
-              queue
-            |> Queue.add
-                 {
-                   pos = Coords.move next.pos left_delta;
-                   dist = next.dist + 1001;
-                   dir = left_dir;
-                   path;
-                 }
-            |> Queue.add
-                 {
-                   pos = Coords.move next.pos right_delta;
-                   dist = next.dist + 1001;
-                   dir = right_dir;
-                   path;
-                 }
-          in
-          (queue, visited, mins)
-      in
-      aux queue visited mins
+      let (location, score), queue = Queue.pop queue in
+      if score > registered_score then (registered_score, all_paths)
+      else if is_end grid location then
+        let registered_score = score in
+        let all_paths =
+          List.fold_left
+            (fun spots coord -> Spots.add coord spots)
+            all_paths location.coords
+        in
+        aux queue visited all_paths registered_score
+      else
+        let prev_score =
+          Visited.find_opt location visited |> Option.value ~default:max_int
+        in
+        let visited, queue =
+          if prev_score >= score then
+            let visited = Visited.add location score visited in
+            let queue =
+              add_if_empty (Location.move location) (score + 1) grid queue
+              |> add_if_empty
+                   (Location.move_clockwise location)
+                   (score + 1001) grid
+              |> add_if_empty
+                   (Location.move_counterclockwise location)
+                   (score + 1001) grid
+            in
+            (visited, queue)
+          else (visited, queue)
+        in
+        aux queue visited all_paths registered_score
   in
-  aux
-    (Queue.singleton { dist = 0; pos = start; dir = East; path = [] })
-    Visited.empty (max_int, Min_paths.empty)
+  aux (Queue.singleton (start, 0)) Visited.empty Spots.empty max_int
 
 let part_1 file =
   let grid = Matrix.parse file in
-  Format.eprintf "@[<v 0>%a@." Matrix.pp grid;
   let start = Matrix.find_start grid in
   let min_score, _ = traverse grid start in
   min_score
-
-module Spots = Set.Make (Coords)
 
 let pp spots ppf t =
   Array.iteri
@@ -224,15 +203,8 @@ let pp spots ppf t =
 
 let part_2 file =
   let grid = Matrix.parse file in
-  (* Format.eprintf "@[<v 0>%a@." Matrix.pp grid; *)
   let start = Matrix.find_start grid in
-  let min_dist, min_paths = traverse grid start in
-  let best_spots =
-    Min_paths.fold
-      (fun (coord, _) acc -> Spots.add coord acc)
-      min_paths Spots.empty
-  in
-  Format.eprintf "@[<v 0>%a@.%d@." (pp best_spots) grid min_dist;
-  Spots.cardinal best_spots
+  let _, spots = traverse grid start in
+  Spots.cardinal spots
 
 let run part file = match part with 1 -> part_1 file | _ -> part_2 file
--- advent_of_code_ocaml/2024/src/days/day17.ml
+++ advent_of_code_ocaml/2024/src/days/day17.ml
@@ -1,3 +1,183 @@
-let part_1 _file = failwith "TODO"
+open Mdrp_lib
+
+type registers = { a : int; b : int; c : int }
+
+let pp_reg ppf { a; b; c } = Format.fprintf ppf "{a: %d; b: %d; c: %d}" a b c
+
+type program = {
+  program : int Array.t;
+  registers : registers;
+  inst_pointer : int;
+  outputs : int list;
+}
+
+let pp ppf { program; registers; inst_pointer; outputs } =
+  Format.fprintf ppf "{prog: %a; reg: %a; ptr: %d; outputs: %a}"
+    Array.(pp Int.pp)
+    program pp_reg registers inst_pointer
+    List.(pp Int.pp)
+    (List.rev outputs)
+
+module Operand = struct
+  type t = int
+
+  let of_int ?registers i =
+    match registers with
+    | None -> i
+    | Some registers -> (
+        match i with
+        | 4 -> registers.a
+        | 5 -> registers.b
+        | 6 -> registers.c
+        | 7 -> raise Exit
+        | i when 0 <= i && i <= 3 -> i
+        | _ -> assert false)
+
+  let debug ?registers i =
+    match registers with
+    | None -> string_of_int i
+    | Some _ -> (
+        match i with
+        | 4 -> "a"
+        | 5 -> "b"
+        | 6 -> "c"
+        | 7 -> raise Exit
+        | i when 0 <= i && i <= 3 -> string_of_int i
+        | _ -> assert false)
+end
+
+module Instr = struct
+  type t =
+    | ADivision0_cmb
+    | Xor1_lit
+    | Bst2_cmb
+    | Jnz3_lit
+    | Bxc4_ign
+    | Out5_cmb
+    | BDivision6_cmb
+    | CDivision7_cmb
+
+  let pp operand registers ppf = function
+    | ADivision0_cmb ->
+        Format.fprintf ppf "a <- a / 2^%s" (Operand.debug ~registers operand)
+    | Xor1_lit -> Format.fprintf ppf "b <- b xor %s" (Operand.debug operand)
+    | Bst2_cmb ->
+        Format.fprintf ppf "b <- %s mod 8" (Operand.debug ~registers operand)
+    | Jnz3_lit -> Format.fprintf ppf "jump if a != 0"
+    | Bxc4_ign -> Format.fprintf ppf "b <- b xor c"
+    | Out5_cmb ->
+        Format.fprintf ppf "output %s" (Operand.debug ~registers operand)
+    | BDivision6_cmb ->
+        Format.fprintf ppf "b <- a / 2^%s" (Operand.debug ~registers operand)
+    | CDivision7_cmb ->
+        Format.fprintf ppf "c <- a / 2^%s" (Operand.debug ~registers operand)
+
+  let next_inst program registers =
+    { program with registers; inst_pointer = program.inst_pointer + 2 }
+
+  let of_int = function
+    | 0 -> ADivision0_cmb
+    | 1 -> Xor1_lit
+    | 2 -> Bst2_cmb
+    | 3 -> Jnz3_lit
+    | 4 -> Bxc4_ign
+    | 5 -> Out5_cmb
+    | 6 -> BDivision6_cmb
+    | 7 -> CDivision7_cmb
+    | _ -> assert false
+
+  let eval program t operand =
+    let registers = program.registers in
+    Format.eprintf "%a@." (pp operand registers) t;
+    let util_div () = 2. ** (float @@ Operand.of_int ~registers operand) in
+    match t with
+    | ADivision0_cmb ->
+        next_inst program
+          {
+            registers with
+            a = float registers.a /. util_div () |> int_of_float;
+          }
+    | Xor1_lit ->
+        next_inst program
+          { registers with b = registers.b lxor Operand.of_int operand }
+    | Bst2_cmb ->
+        next_inst program
+          { registers with b = Operand.of_int ~registers operand mod 8 }
+    | Jnz3_lit ->
+        if registers.a = 0 then
+          { program with inst_pointer = program.inst_pointer + 2 }
+        else { program with inst_pointer = Operand.of_int operand }
+    | Bxc4_ign ->
+        next_inst program { registers with b = registers.b lxor registers.c }
+    | Out5_cmb ->
+        {
+          program with
+          outputs = (Operand.of_int ~registers operand mod 8) :: program.outputs;
+          inst_pointer = program.inst_pointer + 2;
+        }
+    | BDivision6_cmb ->
+        next_inst program
+          {
+            registers with
+            b = float registers.a /. util_div () |> int_of_float;
+          }
+    | CDivision7_cmb ->
+        next_inst program
+          {
+            registers with
+            c = float registers.a /. util_div () |> int_of_float;
+          }
+end
+
+let rec eval program =
+  match program.program.(program.inst_pointer) with
+  | instruction ->
+      let program =
+        Instr.eval program (Instr.of_int instruction)
+          program.program.(program.inst_pointer + 1)
+      in
+      eval program
+  | exception _ ->
+      Format.printf "%a@."
+        Format.(
+          pp_print_list
+            ~pp_sep:(fun ppf () -> Format.fprintf ppf ",")
+            pp_print_int)
+        (List.rev program.outputs)
+
+let part_1 file =
+  let ci = open_in file in
+  let a =
+    input_line ci
+    |> String.split_on_char_non_empty ':'
+    |> List.tl |> List.hd |> String.trim |> int_of_string
+  in
+
+  let b =
+    input_line ci
+    |> String.split_on_char_non_empty ':'
+    |> List.tl |> List.hd |> String.trim |> int_of_string
+  in
+
+  let c =
+    input_line ci
+    |> String.split_on_char_non_empty ':'
+    |> List.tl |> List.hd |> String.trim |> int_of_string
+  in
+
+  ignore (input_line ci);
+  let program =
+    input_line ci
+    |> String.split_on_char_non_empty ':'
+    |> List.tl |> List.hd
+    |> String.split_on_char_non_empty ','
+    |> List.map String.trim |> List.map int_of_string |> Array.of_list
+  in
+  let program =
+    { program; registers = { a; b; c }; inst_pointer = 0; outputs = [] }
+  in
+  eval program;
+  0
+
 let part_2 _file = failwith "TODO"
 let run part file = match part with 1 -> part_1 file | _ -> part_2 file
